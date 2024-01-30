996d7eb0ef7a131dd5e2a4fc67dbf0ce15c622ea
NanyThery <nadinethery@gmail.com>
2024-01-30 23:10:07 +0100

    add first interface and folder

---
+++ src/types/clothes.interface.ts
@@ -0,0 +1,8 @@
+export interface Clothes {
+  id: number;
+  title: string;
+  price: number;
+  comparedPrice: number; //Price to show without discount
+  colors: string[];
+  featuredImage: string;
+}
