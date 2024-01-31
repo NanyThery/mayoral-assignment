8c844415788fcf07aa22446b0d8d5d0f10eb201e
NanyThery <nadinethery@gmail.com>
2024-01-31 12:29:51 +0100

    remove comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from 'next/app';
 import '../styles/global.css';
 import Layout from 'components/Layout/Layout';
 
-// TODO Add layout component for centering and responsive
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
