d3a473aa5f9b9c5ecd757c7f55cdd869d40d6f42
NanyThery <nadinethery@gmail.com>
2024-01-31 12:29:02 +0100

    add components to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,33 @@
+import { ProductGrid } from 'components/ProductGrid/ProductGrid';
+import ZoomButtons from 'components/ZoomButtons/ZoomButtons';
 import { NextPage } from 'next';
+import { useState } from 'react';
 import { Clothes } from 'types/clothes.interface';
 
-const CLOTHES_URL =
-  'https://run.mocky.io/v3/b4d26680-f43c-40f1-b54c-14668f104f60c1856-7cd8-4ba3-884d-b0657b63ba76';
+//TODO move this to a config file
+const CLOTHES_URL = 'https://run.mocky.io/v3/b4d26680-f43c-40f1-b54c-14668f104f60';
 // empty array: https://run.mocky.io/v3/a79c1856-7cd8-4ba3-884d-b0657b63ba76
 // clothes: 'https://run.mocky.io/v3/b4d26680-f43c-40f1-b54c-14668f104f60'
 
-const HomePage: NextPage = ({ data }) => {
+interface HomeProps {
+  data: Clothes[];
+}
+
+const HomePage: NextPage<HomeProps> = ({ data = [] }) => {
+  const [zoomIn, setZoomIn] = useState(true);
   return (
     <div>
-      {data.length > 0 ? 'hay elementos' : 'no hay elementos'}{' '}
-      <p className="small">este mas pequeño</p>
+      <ZoomButtons
+        onZoomIn={() => {
+          !zoomIn && setZoomIn(true);
+        }}
+        onZoomOut={() => {
+          zoomIn && setZoomIn(false);
+        }}
+        isZoomEnabled={zoomIn}
+      />
+      {data.length > 0 ? <ProductGrid products={data} zoomIn={zoomIn} /> : 'no hay elementos'}{' '}
+      <p className="small">todo</p>
       <p className="xSmall">este mas pequeño aun</p>
     </div>
   );
@@ -23,9 +40,9 @@ export const getStaticProps = async () => {
   try {
     const res = await fetch(CLOTHES_URL);
     const data = (await res.json()) as Clothes[];
+
     return { props: { data } };
   } catch (e) {
-    console.log(e);
     return { props: { data: [] } };
   }
 };
