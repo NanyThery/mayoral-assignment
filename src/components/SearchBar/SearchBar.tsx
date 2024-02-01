50200389c369e568752df5518532d3f87cb57d43
NanyThery <nadinethery@gmail.com>
2024-02-01 15:29:24 +0100

    add search functionality and use-debounce library

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,29 +1,33 @@
 import CustomTextInput from '../UI/CustomTextInput/CustomTextInput';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import styles from './SearchBar.module.css';
 import { SearchIcon } from '../UI/Icons/Icons';
-
-interface SearchBarProps {
-  onSearch: (value: SearchTerm) => void;
-}
+import useUpdateParams from 'hooks/useUpdateParams';
+import { useDebouncedCallback } from 'use-debounce';
 
 type SearchTerm = string;
 
-export default function SearchBar({ onSearch = () => {} }: SearchBarProps) {
+export default function SearchBar() {
   const [searchTerm, setSearchTerm] = useState<SearchTerm>('');
+  const { updateUrlParams } = useUpdateParams();
+  const debouncedUpdateUrlParams = useDebouncedCallback(updateUrlParams, 300);
+
+  const handleChange = useCallback(
+    (value: string) => {
+      setSearchTerm(value);
+      debouncedUpdateUrlParams({ query: value });
+    },
+    [setSearchTerm, updateUrlParams],
+  );
 
   return (
     <div className={styles.container}>
-      <form>
-        <CustomTextInput
-          placeholder="Buscar"
-          icon={<SearchIcon />}
-          value={searchTerm}
-          onChange={(term) => {
-            setSearchTerm(term);
-          }}
-        />
-      </form>
+      <CustomTextInput
+        placeholder="Buscar"
+        icon={<SearchIcon />}
+        value={searchTerm}
+        onChange={handleChange}
+      />
     </div>
   );
 }
