4699d664e6dcd2393353639d776ca85150e2ae40
NanyThery <nadinethery@gmail.com>
2024-02-01 15:24:07 +0100

    rearrange button order

--- src/components/ZoomButtons/ZoomButtons.tsx
+++ src/components/ZoomButtons/ZoomButtons.tsx
@@ -4,11 +4,11 @@ import CustomButton from '../UI/CustomButton/CustomButton';
 export default function ZoomButtons({ onZoomIn = () => {}, onZoomOut = () => {}, isZoomEnabled }) {
   return (
     <div>
-      <CustomButton onClick={onZoomIn} disabled={isZoomEnabled} variant="neutral">
-        <PlusIcon />
-      </CustomButton>
       <CustomButton onClick={onZoomOut} disabled={!isZoomEnabled} variant="neutral">
         <MinusIcon />
+      </CustomButton>
+      <CustomButton onClick={onZoomIn} disabled={isZoomEnabled} variant="neutral">
+        <PlusIcon />
       </CustomButton>
     </div>
   );
