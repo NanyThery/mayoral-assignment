e70e78efcb32d0c92605096ca5994441a536d8e6
NanyThery <nadinethery@gmail.com>
2024-02-01 16:45:35 +0100

    add product helper functions

---
+++ src/utils/products.test.js
@@ -0,0 +1,107 @@
+import { filterProducts, mapProducts, sortProducts } from './products';
+import products from '../mocks/products.json';
+
+describe('mapProducts', () => {
+  it('should correctly map product', () => {
+    const rawData = [
+      {
+        id: 1,
+        title: 'Polo banda Better Cotton chico',
+        price: 15.19,
+        comparedPrice: 18.99,
+        colors: ['azul', 'verde', 'rojo'],
+        featuredImage: 'http://someurl.com/image.jpg',
+      },
+    ];
+
+    const mappedProducts = mapProducts(rawData);
+
+    // Check the length of the products
+    expect(mappedProducts.length).toBe(rawData.length);
+
+    // Check the additional properties mapped
+    const firstProduct = mappedProducts[0];
+    expect(firstProduct).toHaveProperty('priceEur');
+    expect(firstProduct).toHaveProperty('comparedPriceEur');
+    expect(firstProduct).toHaveProperty('differencePercentage');
+    expect(firstProduct).toHaveProperty('isReduced');
+  });
+
+  it('should not crash when a property is not included from the api response', () => {
+    const rawData = [
+      {
+        id: 1,
+        title: 'Polo banda Better Cotton chico',
+        price: 15.19,
+        comparedPrice: 18.99,
+      },
+    ];
+
+    const mappedProducts = mapProducts(rawData);
+
+    // Check the length of the products
+    expect(mappedProducts.length).toBe(rawData.length);
+
+    // Check the additional properties mapped
+    const firstProduct = mappedProducts[0];
+    expect(firstProduct).toHaveProperty('colors');
+    expect(firstProduct.colors).toEqual([]);
+    expect(firstProduct).toHaveProperty('featuredImage');
+    expect(firstProduct.featuredImage).toBe('');
+  });
+});
+
+describe('filterProducts', () => {
+  it('should correctly filter products', () => {
+    const rawData = products;
+
+    const filteredProducts = filterProducts(rawData, 'unique');
+
+    expect(filteredProducts.length).toBe(1);
+    expect(filteredProducts[0].title).toBe('This unique product for testing');
+  });
+});
+
+describe('sortProducts', () => {
+  it('should correctly sort products in ascending order', () => {
+    const products = [
+      { title: 'Product B', price: 200 },
+      { title: 'Product A', price: 100 },
+      { title: 'Product C', price: 300 },
+    ];
+
+    const sortedProducts = sortProducts(products, 'priceAsc');
+
+    expect(sortedProducts[0].title).toBe('Product A');
+    expect(sortedProducts[1].title).toBe('Product B');
+    expect(sortedProducts[2].title).toBe('Product C');
+  });
+
+  it('should correctly sort products in descending order', () => {
+    const products = [
+      { title: 'Product B', price: 200 },
+      { title: 'Product A', price: 100 },
+      { title: 'Product C', price: 300 },
+    ];
+
+    const sortedProducts = sortProducts(products, 'priceDesc');
+
+    expect(sortedProducts[0].title).toBe('Product C');
+    expect(sortedProducts[1].title).toBe('Product B');
+    expect(sortedProducts[2].title).toBe('Product A');
+  });
+
+  it('should return the same order when the sort order is not valid or empty', () => {
+    const products = [
+      { title: 'Product B', price: 200 },
+      { title: 'Product A', price: 100 },
+      { title: 'Product C', price: 300 },
+    ];
+
+    const sortedProducts = sortProducts(products, null);
+
+    expect(sortedProducts[0].title).toBe('Product B');
+    expect(sortedProducts[1].title).toBe('Product A');
+    expect(sortedProducts[2].title).toBe('Product C');
+  });
+});
---
+++ src/utils/products.tsx
@@ -0,0 +1,68 @@
+import formatEuro from './formatEuro';
+import calculateDifferencePercentage from './calculateDifferencePercentage';
+import { Clothes } from '../types/clothes.interface';
+
+const CLOTHES_URL = process.env.API_URL;
+// empty array: https://run.mocky.io/v3/a79c1856-7cd8-4ba3-884d-b0657b63ba76
+// clothes: 'https://run.mocky.io/v3/b4d26680-f43c-40f1-b54c-14668f104f60'
+
+export async function fetchProducts() {
+  const res = await fetch(CLOTHES_URL);
+  const data = (await res.json()) as Clothes[];
+  return data;
+}
+
+export function mapProducts(products: Clothes[]) {
+  return products.map((product) => {
+    const isReduced = product.comparedPrice > product.price;
+    return {
+      id: product.id || null,
+      title: product.title || '',
+      price: product.price || 0,
+      featuredImage: product.featuredImage || '',
+      isReduced,
+      priceEur: formatEuro(product.price),
+      comparedPrice: product.comparedPrice || 0,
+      comparedPriceEur: (product.comparedPrice && formatEuro(product.comparedPrice)) || '0',
+      differencePercentage:
+        (isReduced && calculateDifferencePercentage(product.price, product.comparedPrice)) || '',
+      colors: product.colors || [],
+    };
+  });
+}
+
+export async function fetchAllProducts(): Promise<Clothes[]> {
+  const rawProducts = await fetchProducts();
+  const products = mapProducts(rawProducts);
+  return products;
+}
+
+export async function searchProducts(searchParams: URLSearchParams): Promise<Clothes[]> {
+  const query = searchParams.get('query');
+  const sort = searchParams.get('sort');
+
+  const rawProducts = await fetchProducts();
+  const mappedProducts = mapProducts(rawProducts);
+
+  // This emulates what an external API would do before sending the response
+  const filteredProducts = filterProducts(mappedProducts, query);
+  const sortedProducts = sortProducts(filteredProducts, sort);
+
+  return sortedProducts;
+}
+
+export function filterProducts(products: Clothes[], query: string): Clothes[] {
+  if (!query) return products;
+  return products.filter((product) => product.title.toLowerCase().includes(query.toLowerCase()));
+}
+
+export function sortProducts(products: Clothes[], sort: string) {
+  switch (sort) {
+    case 'priceAsc':
+      return products.sort((a, b) => a.price - b.price);
+    case 'priceDesc':
+      return products.sort((a, b) => b.price - a.price);
+    default:
+      return products;
+  }
+}
