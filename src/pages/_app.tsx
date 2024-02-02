f92b27eb01fa1a343f1b84ea22fb3ea5a0c74b66
NanyThery <nadinethery@gmail.com>
2024-02-02 12:27:35 +0100

    add config and title to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
 import Layout from '../components/UI/Layout/Layout';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>NT - Mayoral Assigment</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
